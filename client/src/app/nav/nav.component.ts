ecf6f89fc2023f98d0b797291a0defe615d29557
Keith Laptop <Keithblack4290@gmail.com>
2025-06-18 19:44:44 -0600

    welcome user logout func

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountsService } from '../_services/accounts.service';
-import { NgIf } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
