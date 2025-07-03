77ce6a5cc4cff9cb15d5c325b13081d3e9c5a706
Keith Laptop <Keithblack4290@gmail.com>
2025-07-03 12:33:40 -0600

    updating using tailwind and daisy

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4169890bec59a31358b39694c30386049538bd84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61692be21441c059d0aaec168424678b77f27496")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
