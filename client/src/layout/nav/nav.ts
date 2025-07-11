a1b673d7d56de084c0ba8a35d7099bd5aa6a94b4
Keith Laptop <Keithblack4290@gmail.com>
2025-07-11 15:56:00 -0600

    added routing

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -3,28 +3,36 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AccountService } from '../../core/services/account-service';
 import { User } from '../../types/userTypes';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
+  private router = inject(Router);
+  private toast = inject(ToastService);
   protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: (response) => {
-        console.log(response);
+        this.router.navigateByUrl('/members');
+        this.toast.success('Logged in successfully');
         this.creds = {};
       },
-      error: (error) => alert(error.message),
+      error: (error) =>{
+        this.toast.error(error.error)
+      }
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
